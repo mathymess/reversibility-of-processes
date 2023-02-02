2a97975b33f7e32506567be4f07bfa999c5f3998
Nikolai Gruzinov <ngruzinov@constructor.university>
2023-02-02 18:30:56 +0100

    Add chop_time_series_into_windows function

--- datagen.py
+++ datagen.py
@@ -16,30 +16,49 @@ def harmonic_oscillator(amplitude: float = 1.,
         t += delta_t
 
 
-def pull_data(generator: Generator[float, None, None],
+def pull_data(generator: Generator[NDArray | float, None, None],
               n_points: int = 10000) -> NDArray:
     return np.fromiter(generator, dtype=float, count=n_points)
 
+def chop_time_series_into_windows(data: NDArray,
+                                  window_len: int = 40,
+                                  target_len: int = 1) -> Tuple[NDArray, NDArray]:
+    assert data.ndim <= 2, "Time series expected, each datapoint is either a number or a 1D array"
+
+    windows = np.array([data[i:i+window_len]
+                        for i in range(len(data) - window_len - target_len + 1)])
+
+    targets = np.array([data[i:i+target_len]
+                        for i in range(window_len, len(data) - target_len + 1)])
+
+    assert len(windows) == len(targets)
+    return windows, targets
+
 
 class TimeSeriesDataset(torch.utils.data.Dataset):
-    def __init__(self, data: NDArray,
-                 window_size: int = 40,
-                 prediction_size: int = 1,
-                 is_reversed: bool = False) -> None:
+    def __init__(self, windows: NDArray, targets: NDArray) -> None:
+        assert len(windows) > 0 and len(targets) > 0
+        assert len(windows) == len(targets)
+
         super().__init__()
 
-        self.data: NDArray = data
-        self.window_size: int = window_size
-        self.prediction_size: int = prediction_size
-        self.is_reversed: bool = is_reversed
-
-    def __len__(self) -> int:
-        return len(self.data)
+        self.windows = torch.from_numpy(windows)
+        self.targets = torch.from_numpy(targets)
+        self.n_points: int = len(windows)
 
     def __get_item__(self, index: int) -> Tuple[NDArray, NDArray]:
-        return (np.array([1.]), np.array([1.]))
+        return (windows[index], targets[index])
 
+    def __len__(self) -> int:
+        return self.n_points
+
+    def datapoint_torch_size(self):
+        return self.windows[0][0]
 
 if __name__ == "__main__":
     osc = harmonic_oscillator()
-    lol: NDArray = pull_data(osc)
+    t: NDArray = pull_data(osc, n_points=5)
+    windows, targets = chop_time_series_into_windows(t, window_len=3, target_len=1)
+    print(t)
+    print(windows)
+    print(targets)
