ed4a2526b3b986ae421cc8f1a98e391fb4fc38b9
Nikolai Gruzinov <ngruzinov@constructor.university>
2023-03-05 23:18:32 +0100

    Improve main.py, hyperparameter grid for lrz

--- main.py
+++ main.py
@@ -48,6 +48,7 @@ def get_mean_loss_on_test_dataset(model: MyModel,
 
 def train(model: MyModel,
           dataloader: torch.utils.data.DataLoader,
+          test_dataset: torch.utils.data.Dataset,
           num_epochs: int = 20,
           tensorboard_dir_suffix: str = "") -> None:
     tensorboard_dir = "runs/" + time.strftime("%Y%m%d_%H%M%S") + "." + tensorboard_dir_suffix
@@ -68,13 +69,13 @@ def train(model: MyModel,
 
             optim.step()
 
-        mean_test_loss = get_mean_loss_on_test_dataset(model, dh.forward.test_dataset)
+        mean_test_loss = get_mean_loss_on_test_dataset(model, test_dataset)
         writer.add_scalar("loss_on_test",  mean_test_loss, epoch)
 
     writer.close()
 
 
-if __name__ == "__main__":
+def train_test_two_body_problem():
     window_len = 30
     dh = load_two_body_problem_dataholder(chunk_len=window_len+1,
                                           shift_ratio=0.9)
@@ -84,20 +85,49 @@ if __name__ == "__main__":
 
     train(MyModel(window_len=window_len, datapoint_size=2),
           dh.forward.train_loader,
+          dh.forward.test_dataset,
           num_epochs=20,
           tensorboard_dir_suffix="kepler_forward")
 
     train(MyModel(window_len=window_len, datapoint_size=2),
           dh.backward.train_loader,
+          dh.backward.test_dataset,
           num_epochs=20,
           tensorboard_dir_suffix="kepler_backward")
 
-# model = MyModel(window_len=window_len, datapoint_size=3)
-# train(model, d_train_dl, tensorboard_dir_suffix="direct")
-# print("Trained without reverse, loss on the test dataset:",
-#       test(model, d_test_dl))
-#
-# model = MyModel(window_len=window_len, datapoint_size=3)
-# train(model, rev_train_dl, tensorboard_dir_suffix="reverse")
-# print("Trained with reverse, loss on the test dataset:",
-#       test(model, rev_test_dl))
+
+def train_test_lorenz_attractor(window_len: int, shift_ratio: float) -> None:
+    dh = load_two_body_problem_dataholder(chunk_len=window_len+1,
+                                          shift_ratio=0.9)
+    # plot_3d_data(dh.train_ts, dh.test_ts, show=True, title="Lorenz time series, train/test")
+    # plot_data_componentwise(dh.train_ts, dh.test_ts, draw_window_len=40,
+    #                         show=True, title="Lorenz time series, train/test")
+
+    train(MyModel(window_len=window_len, datapoint_size=2),
+          dh.forward.train_loader,
+          dh.forward.test_dataset,
+          num_epochs=20,
+          tensorboard_dir_suffix=f"lorenz_forward_shift={shift_ratio}_window={window_len}")
+
+    train(MyModel(window_len=window_len, datapoint_size=2),
+          dh.backward.train_loader,
+          dh.forward.test_dataset,
+          num_epochs=20,
+          tensorboard_dir_suffix=f"lorenz_backward_shift={shift_ratio}_window={window_len}")
+
+
+if __name__ == "__main__":
+    train_test_lorenz_attractor(window_len=30, shift_ratio=0.1)
+    train_test_lorenz_attractor(window_len=30, shift_ratio=0.4)
+    train_test_lorenz_attractor(window_len=30, shift_ratio=0.7)
+    train_test_lorenz_attractor(window_len=30, shift_ratio=0.9)
+
+    train_test_lorenz_attractor(window_len=70, shift_ratio=0.1)
+    train_test_lorenz_attractor(window_len=70, shift_ratio=0.4)
+    train_test_lorenz_attractor(window_len=70, shift_ratio=0.7)
+    train_test_lorenz_attractor(window_len=70, shift_ratio=0.9)
+
+    train_test_lorenz_attractor(window_len=110, shift_ratio=0.1)
+    train_test_lorenz_attractor(window_len=110, shift_ratio=0.4)
+    train_test_lorenz_attractor(window_len=110, shift_ratio=0.7)
+    train_test_lorenz_attractor(window_len=110, shift_ratio=0.9)
