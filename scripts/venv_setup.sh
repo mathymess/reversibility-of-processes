1a48c5e17ecf03ad373c5083c5b776b2900f05b1
Nikolai Gruzinov <ngruzinov@constructor.university>
2023-03-19 20:44:26 +0100

    Add requirements.txt and .flake8

--- scripts/venv_setup.sh
+++ scripts/venv_setup.sh
@@ -1,7 +1,4 @@
 python -m venv .venv
 ./.venv/bin/activate
 pip install --upgrade pip
-pip install numpy scipy matplotlib scikit-learn
-# pip install ipython IPythonClipboard
-pip install torch torchvision
-pip install tensorboard tensorflow
+pip install --requirement requirements.txt
