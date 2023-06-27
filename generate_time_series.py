Fix overflow in Henon map, rerun

generate_time_series.py
@@ -321,7 +321,7 @@ def load_logistic_map_time_series(length: int, coef_a: float = 4.0, x_initial: f
 
 
 def load_henon_map_time_series(length: int, coef_a: float = 1.4, coef_b: float = 0.3,
-                               x_initial: float = 0.6, y_initial: float = 1):
+                               x_initial: float = 0.6, y_initial: float = 0.6):
     x = np.zeros(length)
     y = np.zeros(length)
     x[0] = x_initial
@@ -358,12 +358,12 @@ def load_garch_time_series(length: int, coef_alpha: float = 1,
 
 
 if __name__ == "__main__":
-    # explore_two_body_time_series()
-    # explore_lorenz_attractor_time_series()
-    # explore_belousov_zhabotinsky_time_series()
-    # explore_double_pendulum_time_series()
-    # explore_harmonic_oscillator_time_series()
-    # plot_data_componentwise(load_logistic_map_time_series(1000))
-    # plot_data_componentwise(load_henon_map_time_series(100, x_initial=0.6, y_initial=0))
+    explore_two_body_time_series()
+    explore_lorenz_attractor_time_series()
+    explore_belousov_zhabotinsky_time_series()
+    explore_double_pendulum_time_series()
+    explore_harmonic_oscillator_time_series()
+    plot_data_componentwise(load_logistic_map_time_series(1000))
+    plot_data_componentwise(load_henon_map_time_series(4000, x_initial=0.6))
     plot_data_componentwise(load_arnold_map_time_series(1000))
-    # plot_data_componentwise(load_garch_time_series(10000, coef_alpha=0.9))
+    plot_data_componentwise(load_garch_time_series(10000, coef_alpha=0.9))

traintest_zanin_time_series.py
@@ -30,16 +30,16 @@ def traintest_henon_map():
     hen = load_henon_map_time_series(length=2000)
     filepath = "20230626_distributions/henon.json"
     train_test_distribution(hen, num_runs=100,
-                            window_len=7, hidden_size=20, num_epochs=30,
+                            window_len=5, hidden_size=20, num_epochs=30,
                             save_output_to_file=filepath)
     print(filepath)
 
     np.random.seed(45)
     collection = [load_henon_map_time_series(length=1500, x_initial=x, y_initial=y) for x, y
-                  in np.random.uniform(0, 1, size=200).reshape(-1, 2)]
+                  in np.random.uniform(0.1, 0.9, size=200).reshape(-1, 2)]
     filepath = "20230626_distributions/henon_montecarlo.json"
     train_test_distribution_montecarlo_ts(collection,
-                                          window_len=7, hidden_size=15,
+                                          window_len=5, hidden_size=20,
                                           datapoint_size=2, num_epochs=30,
                                           save_output_to_file=filepath)
     print(filepath)
@@ -84,7 +84,7 @@ def traintest_garch():
 
 
 if __name__ == "__main__":
-    traintest_logistic_map()
+    # traintest_logistic_map()
     traintest_henon_map()
-    traintest_arnold_map()
-    traintest_garch()
+    # traintest_arnold_map()
+    # traintest_garch()