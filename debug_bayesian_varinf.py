bef72d0466ab95c0bdafc9e8eeafba9f09791c69
Nikolai Gruzinov <ngruzinov@constructor.university>
2023-09-04 13:22:11 +0200

    Bad forecasts. varinf, change size to 20

--- debug_bayesian_varinf.py
+++ debug_bayesian_varinf.py
@@ -16,13 +16,13 @@ from bayesian_varinf import (ExpResultsWithTwoLosses,
 if __name__ == "__main__":
     ts = np.linspace(0., 1., 300).reshape(-1, 1)
     d = BayesTrainData(ts, window_len=1, noise_std=0.00005)
-    print(d.windows_f)
 
-    save_dir = "20230724_preds/debug_varinf/linspace_varinf_size8/"
+    save_dir = "20230724_preds/debug_varinf/linspace_varinf_size20/"
 
     if not os.path.isdir(save_dir):
         posterior_predictive_forward_and_backward(train_d=d, save_dir=save_dir,
-                                                  num_samples=60, hidden_size=8)
+                                                  num_samples=60,
+                                                  hidden_size=20)
     else:
         print(f"Directory '{save_dir}' exists, won't overwrite")
 
